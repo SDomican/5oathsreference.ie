e12723fccdd10b3641e8a3f04f2a899b676c6d1f
Domo230 <stephen.p.domican@gmail.com>
2023-07-08 08:46:18 +0100

    Added popup fade

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function showHide() {
     var x = document.getElementById("popupId");
+    var bodyElement = document.getElementById("body");
+
+    if(bodyElement.classList.contains("darken")){
+      bodyElement.classList.remove("darken");
+    }
+    else {
+      bodyElement.classList.add("darken");
+    }
+
+
     if (x.style.display === "none") {
       x.style.display = "block";
-    } else {
+    }
+    else {
       x.style.display = "none";
     }
   }
