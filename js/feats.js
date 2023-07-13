75c37075be9527002ab60916ec799722bd4a2a5b
Domo230 <stephen.p.domican@gmail.com>
2023-07-13 19:29:52 +0100

    Updates to Feats HTML

--- js/feats.js
+++ js/feats.js
@@ -5,45 +5,45 @@ function addSectionContainer(idToAmend){
 }
 
 function apendText(featPath){
-  // document.getElementById("featsContainer").innerHTML +=
-  // "<div class=\"section-title\"><span class=\"float-right\">" + featPath.pathName + "</span></div>";
-
-
-  document.getElementById("featsContainer").innerHTML +=
-  "<div class=\"section-title\">" +
-    "<span class=\"float-right\">Way of Leather and Steel </span>" +
-  "</div>"
-  "<div class=\"section-content\">" + 
-    "<div class=\"section-row section-subtitle text fontsize\">" +
-      "You can move at any time during your turn (before, after, or during actions)." +
-  "</div>" +
-  "<div class=\"section-row\">";
-
 
   var featsArray = featPath.feats;
+  var pathName = featPath.pathName;
+  console.log(pathName);
 
-  // document.getElementById("featsContainer").innerHTML += "<div class=\"section-content\"><div class=\"section-row section-subtitle text fontsize\">You can move at any time during your turn (before, after, or during actions).</div><div class=\"section-row\">";
-
-  featsArray.forEach(feat => {
-    
-    var featName = feat.featName;
-    console.log("LOG: " + featName)
-
-  //   document.getElementById("featsContainer").innerHTML +=
-  // "<div class=\"item itemsize\"><div class=\"item-icon iconsize icon-run\" style=\"background-color: brown;\" onclick=\"showHide()\" ></div><div class=\"item-text-container text\"><div class=\"item-title\">Weapon blow</div><div class=\"item-desc\">Cost: 5ft per 5ft</div></div>";
-  // });
-
-  document.getElementById("featsContainer").innerHTML +=
-  "<div class=\"item itemsize\">" +
-  "<div class=\"item-icon iconsize icon-run\" style=\"background-color: brown;\" onclick=\"showHide()\" ></div>" +
-  "<div class=\"item-text-container text\">" +
-      "<div class=\"item-title\">Weapon blow</div>" +
-      "<div class=\"item-desc\">Cost: 5ft per 5ft</div>" +
+  var featCode = "<div class=\"section-container\">" + 
+  "<div class=\"section-title\">" +
+      "<span class=\"float-right\">" + pathName + "</span>" +
   "</div>" +
-"</div>"
-  });
+  "<div class=\"section-content\">" +
+      "<div class=\"section-row section-subtitle text fontsize\">" +
+          "You can move at any time during your turn (before, after, or during actions)." +
+      "</div>" +
+      "<div class=\"section-row\">";
+      
+      featsArray.forEach(feat => {
 
-  document.getElementById("featsContainer").innerHTML += "</div></div>";
+        var featName = feat.featName;
+        var featRequirements = feat.prequisites;
+
+        console.log(featRequirements);
+
+        featCode +=
+        "<div class=\"item itemsize\">" +
+          "<div class=\"item-icon iconsize icon-run\" style=\"background-color: brown;\" onclick=\"showHide()\" ></div>" +
+          "<div class=\"item-text-container text\">" +
+            "<div class=\"item-title\">" + featName + "</div>" +
+            "<div class=\"item-desc\">Prerequisites: " + featRequirements + "</div>" +
+        "</div>" +
+    "</div>";
+      });
+
+    featCode +=   
+  "</div>" +
+  "</div>" +
+"</div>";
+
+document.getElementById("featsContainer").innerHTML += featCode;
+
 }
 
 // function fetch(){
